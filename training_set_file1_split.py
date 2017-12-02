e0c2052d3591be35d2ffaff7b8459c1049e30477
Prabhakar Nanduri <prabhakar.nanduri@duke.edu>
2017-12-01 20:44:42 -0500

    analysis on training file sets

---
+++ training_set_file1_split.py
@@ -0,0 +1,46 @@
+
+# coding: utf-8
+
+# In[10]:
+
+
+import pandas as pd
+
+
+# In[11]:
+
+
+raw_file=pd.read_csv('file_1_part1.csv',index_col='time')
+
+
+# In[12]:
+
+
+test_file1_1=raw_file[0:1000]
+test_file1_2=raw_file[1000:2000]
+test_file1_3=raw_file[2000:3000]
+test_file1_4=raw_file[3000:4000]
+test_file1_5=raw_file[4000:5000]
+test_file1_6=raw_file[5000:6000]
+test_file1_7=raw_file[6000:7000]
+test_file1_8=raw_file[7000:8000]
+test_file1_9=raw_file[8000:9000]
+test_file1_10=raw_file[9000:10000]
+test_file1_11=raw_file[10000:11000]
+test_file1_12=raw_file[11000:1200]
+test_file1_13=raw_file[12000:13000]
+test_file1_14=raw_file[13000:14000]
+test_file1_15=raw_file[14000:15000]
+test_file1_16=raw_file[15000:16000]
+test_file1_17=raw_file[16000:17000]
+test_file1_18=raw_file[17000:18000]
+test_file1_19=raw_file[18000:19000]
+test_file1_20=raw_file[19000:20000]
+test_file1_21=raw_file[20000:21000]
+
+
+# In[14]:
+
+
+test_file1_2
+
